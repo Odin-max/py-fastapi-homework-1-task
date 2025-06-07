b0a038a9c4125837d7a747e124acf585a74cc4df
Odin-max <meh84741@gmail.com>
2025-06-07 19:44:55 +0300

    1

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,59 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import func
+from src.schemas.movies import MovieDetailResponseSchema, MovieListResponseSchema
 
 from database import get_db, MovieModel
 
 
 router = APIRouter()
 
-# Write your code here
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movies(
+    page: int = Query(1, ge=1),
+    per_page: int = Query(10, ge=1, le=20),
+    db: AsyncSession = Depends(get_db)
+):
+    offset = (page - 1) * per_page
+
+    total_items_result = await db.execute(select(func.count()).select_from(MovieModel))
+    total_items = total_items_result.scalar_one()
+
+    if total_items == 0:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    total_pages = (total_items + per_page - 1) // per_page
+
+    result = await db.execute(
+        select(MovieModel)
+        .offset(offset)
+        .limit(per_page)
+    )
+    movies = result.scalars().all()
+
+    base_url = "/theater/movies/"
+    prev_page = f"{base_url}?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"{base_url}?page={page + 1}&per_page={per_page}" if page < total_pages else None
+
+    return MovieListResponseSchema(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items
+    )
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie_by_id(
+    movie_id: int,
+    db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(select(MovieModel).where(MovieModel.id == movie_id))
+    movie = result.scalar_one_or_none()
+
+    if movie is None:
+        raise HTTPException(status_code=404, detail="Movie with the given ID was not found.")
+
+    return movie
